c24870ce0a4538719f598e12ba78b1a2b48ca227
tyssos <sergio@tyssos.com>
2026-03-18 23:04:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const fetch = (...args) => import("node-fetch").then(({default: fetch}) => fetch(...args));
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
